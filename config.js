d5a6c89feacecdc890d2d80e29abcc670e044c92
Facundo Ameal <me@fameal.com.ar>
2016-03-08 18:25:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var monitors = [
-    "apikey1",  //Place your first Key Here
-    "apikey2", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
+    "m777318984-d852313d8b341c6e46cc7cbd",  //Place your first Key Here
+    "m777475077-4367b1c40385b2db9a4bf7ff", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
 
     
 ];
-var title = 'My Project Status Page'; //Page <title> and heading at top of page
+var title = 'FA - Status Page'; //Page <title> and heading at top of page
 var refreshtime = 60; //How often to refresh in seconds
 var footermessage = ''; //HTML to go in footer
